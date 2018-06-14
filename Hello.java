ccc26e959401e38de33a2b7f4aba786df17fa80c
jyothi <jyothi@gmail.com>
2018-06-14 15:59:26 -0400

    Release1 is modified the Hello.java file which from tag v1.0

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello{
    public static void main( String args[]){
 
    System.out.println(" This Hello.Java file edited in the Release1 branch");
- }
+   System.out.println(" This is from Release1 branch which is created from tag");` 
+}
 }
 
